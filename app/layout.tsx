20db382feaa9cd14a82b6f72668174789e998a5a
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-07-25 13:45:32 +0530

    Favicon icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata = {
 const RootLayout = ({ children }: any) => {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="favicon.svg" />
+      </head>
       <body>
         <Provider>
           <div className="main">
